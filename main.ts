b95a415d3db43317b877d6d0c1bc6c0423154851
Atlinx <atlinuxtm@gmail.com>
2023-05-16 15:55:49 -0400

    Add debugging

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 function prependBase() {
+  console.log("try prepend base");
   document.querySelectorAll("a").forEach((link) => {
+    console.log("found link ", link);
     let url = link.getAttribute("href");
     if (url?.startsWith("/")) {
       url = import.meta.env.BASE_URL + url.slice(1);
@@ -8,6 +10,7 @@ function prependBase() {
   });
 
   document.querySelectorAll("a-asset-item").forEach((item) => {
+    console.log("found asset-item ", item);
     let url = item.getAttribute("src");
     if (url?.startsWith("/")) {
       url = import.meta.env.BASE_URL + url.slice(1);
