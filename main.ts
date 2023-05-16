e417acdeddc6c69cbf75f2df8caf7435069156a5
Atlinx <atlinuxtm@gmail.com>
2023-05-16 15:47:07 -0400

    Also replace asest items

--- main.ts
+++ main.ts
@@ -6,4 +6,12 @@
       link.setAttribute("href", url);
     }
   });
+
+  document.querySelectorAll("a-asset-item").forEach((item) => {
+    let url = item.getAttribute("src");
+    if (url?.startsWith("/")) {
+      url = import.meta.env.BASE_URL + url.slice(1);
+      item.setAttribute("src", url);
+    }
+  });
 })();
