fd56248c594c536877531ea3ce42a3eb76b2bfca
Dhawal Arora <119643277+dhawal-arora@users.noreply.github.com>
2023-06-15 11:37:52 -0400

    gltf-->glb site6

--- pages/sites/site-6.vue
+++ pages/sites/site-6.vue
@@ -14,7 +14,27 @@ async function loadSystems() {
     <a-sun-sky material="sunPosition: -0.2 4 -5"></a-sun-sky>
     <a-entity
           position="0 0 0"
-          gltf-model="/assets/models/site6/Person1.glb"
+          gltf-model="/assets/sites/site6/Person1.glb"
+    ></a-entity>
+    <a-entity
+          position="6 6 6"
+          gltf-model="/assets/sites/site6/Person2.glb"
+    ></a-entity>
+    <a-entity
+          position="2 2 2"
+          gltf-model="/assets/sites/site6/Person3.glb"
+    ></a-entity>
+    <a-entity
+          position="3 3 3"
+          gltf-model="/assets/sites/site6/Person4.glb"
+    ></a-entity>
+    <a-entity
+          position="4 4 4"
+          gltf-model="/assets/sites/site6/Person5.glb"
+    ></a-entity>
+    <a-entity
+          position="5 5 5"
+          gltf-model="/assets/sites/site6/Person6.glb"
     ></a-entity>
     <a-entity camera look-controls wasd-controls position="0 1.6 0">
       <a-cursor
