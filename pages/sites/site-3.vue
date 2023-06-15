2e642402aeccc2ce2669c1c6dd993fd4346b258e
Dhawal Arora <119643277+dhawal-arora@users.noreply.github.com>
2023-06-15 12:06:32 -0400

    site3&site6 demo ready

--- pages/sites/site-3.vue
+++ pages/sites/site-3.vue
@@ -6,6 +6,8 @@ definePageMeta({
 async function loadSystems() {
   //@ts-ignore
   await import('aframe-sun-sky');
+  await import('@/aframe/components/button');
+  await import('@/aframe/components/audio-player');
 }
 </script>
 
@@ -13,7 +15,7 @@ async function loadSystems() {
   <AFrameScene :load-systems="loadSystems">
     <a-sun-sky material="sunPosition: -0.2 4 -5"></a-sun-sky>
     <a-assets>
-      <!-- <video
+    <video
         id="trash-video"
         preload="auto"
         width="160"
@@ -21,22 +23,26 @@ async function loadSystems() {
         loop="true"
         :src="usePublic('assets/sites/site3/trash-video.mp4')"
         crossorigin="anonymous"
-      ></video> -->
+    ></video>
       <audio
         id="interview"
         :src="usePublic('assets/sites/site3/Site 3 Final Audio.mp3')"
       ></audio>
     </a-assets>
-    <!-- <a-playback-video
+    <a-playback-video
       src="#trash-video"
       position="0 0 -20"
       rotation="0 0 0"
-    ></a-playback-video> -->
+    ></a-playback-video>
     <a-entity
-      sound="src: #interview; autoplay: true; loop: true; rolloffFactor: 8;"
       position="0 0 0"
       :gltf-model="usePublic('assets/sites/site3/boat.glb')"
     ></a-entity>
+    <a-audio-player
+      src="#interview"
+      position="0 1.6 -5"
+      title="Interview"
+    ></a-audio-player>
     <a-entity camera look-controls wasd-controls position="0 1.6 0">
       <a-animated-cursor></a-animated-cursor>
     </a-entity>
--- pages/sites/site-6.vue
+++ pages/sites/site-6.vue
@@ -6,12 +6,18 @@ definePageMeta({
 async function loadSystems() {
   //@ts-ignore
   await import('aframe-sun-sky');
+  await import('@/aframe/components/button');
+  await import('@/aframe/components/audio-player');
 }
 </script>
 
 <template>
   <AFrameScene :load-systems="loadSystems">
     <a-sun-sky material="sunPosition: -0.2 4 -5"></a-sun-sky>
+    <a-assets>
+      <!-- <audio id="interview" :src="usePublic('assets/site6/interview6.mp3')"></audio> -->
+      <audio id="interview" :src="usePublic('assets/sites/site6/interview6.mp3')" preload="auto"></audio>
+    </a-assets>
     <a-entity
           position="0 0 0"
           gltf-model="/assets/sites/site6/Person1.glb"
@@ -36,17 +42,13 @@ async function loadSystems() {
           position="5 5 5"
           gltf-model="/assets/sites/site6/Person6.glb"
     ></a-entity>
+    <a-audio-player
+      src="#interview"
+      position="0 1.6 -5"
+      title="Interview"
+    ></a-audio-player>
     <a-entity camera look-controls wasd-controls position="0 1.6 0">
-      <a-cursor
-        id="cursor"
-        color="white"
-        rayOrigin="mouse"
-        raycaster="objects: .clickable"
-        fuseTimeout="500"
-        animation__click="property: scale; startEvents: click; easing: easeInCubic; dur: 150; from: 0.1 0.1 0.1; to: 1 1 1"
-        animation__fusing="property: scale; startEvents: fusing; easing: easeInCubic; dur: 1500; from: 1 1 1; to: 0.1 0.1 0.1"
-        animation__mouseleave="property: scale; startEvents: mouseleave; easing: easeInCubic; dur: 500; to: 1 1 1"
-      ></a-cursor>
+      <a-animated-cursor></a-animated-cursor>
     </a-entity>
   </AFrameScene>
 </template>
