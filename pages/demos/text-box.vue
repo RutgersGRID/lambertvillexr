26bf8ee06538554ab7d9cc04e2716c7cb2b62778
Atlinx <atlinuxtm@gmail.com>
2023-06-27 11:28:57 -0400

    Improve text-box demo

--- pages/demos/text-box.vue
+++ pages/demos/text-box.vue
@@ -10,46 +10,75 @@ async function loadSystems() {
   <AFrameScene :load-systems="loadSystems">
     <a-sun-sky material="sunPosition: -0.2 4 -5"></a-sun-sky>
 
-    <a-text-box
-      width="2"
-      height="2"
-      title="Only Title"
-      position="-3 0 -5"
-    ></a-text-box>
-    <a-text-box
-      width="2"
-      height="2"
-      description="This is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text,"
-      position="0 0 -5"
-    ></a-text-box>
-    <a-text-box
-      width="2"
-      height="2"
-      title="Some Title"
-      description="This is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text,"
-      position="3 0 -5"
-    ></a-text-box>
+    <a-entity position="0 -1.5 -1">
+      <!-- First Row -->
+      <a-text-box
+        width="2"
+        height="2"
+        title="Only Title"
+        position="-3 0 -5"
+      ></a-text-box>
+      <a-text-box
+        width="2"
+        height="2"
+        description="This is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text,"
+        position="0 0 -5"
+      ></a-text-box>
+      <a-text-box
+        width="2"
+        height="2"
+        title="Some Title"
+        description="This is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text, this is some text,"
+        position="3 0 -5"
+      ></a-text-box>
 
-    <a-text-box
-      width="2"
-      height="2"
-      title="Only Title"
-      autofit-height="false"
-      position="-3 3 -5"
-    ></a-text-box>
-    <a-text-box
-      width="2"
-      height="2"
-      description="This is some text, this is some text"
-      position="0 3 -5"
-    ></a-text-box>
-    <a-text-box
-      width="2"
-      height="2"
-      title="Some Title"
-      description="This is some text, this is some text "
-      position="3 3 -5"
-    ></a-text-box>
+      <!-- Second Row -->
+      <a-text-box
+        width="2"
+        height="2"
+        title="Only Title"
+        autofit-height="false"
+        position="-3 3 -5"
+      ></a-text-box>
+      <a-text-box
+        width="2"
+        height="2"
+        description="This is some text, this is some text"
+        position="0 3 -5"
+      ></a-text-box>
+      <a-text-box
+        width="2"
+        height="2"
+        title="Some Title"
+        description="This is some text, this is some text "
+        position="3 3 -5"
+      ></a-text-box>
+
+      <!-- Third Row -->
+      <a-text-box
+        width="2"
+        height="2"
+        title="Some Title"
+        description="This is some text, this is some text This is some text, this is some text This is some text, this is some text This is some text, this is some text "
+        autofit-height="false"
+        position="-3 6 -5"
+      ></a-text-box>
+      <a-text-box
+        width="2"
+        height="2"
+        title="Some Title"
+        description="This is some text, this is some text "
+        autofit-height="false"
+        position="0 6 -5"
+      ></a-text-box>
+      <a-text-box
+        width="2"
+        height="2"
+        description="This is some text, this is some text "
+        autofit-height="false"
+        position="3 6 -5"
+      ></a-text-box>
+    </a-entity>
 
     <a-entity
       camera
